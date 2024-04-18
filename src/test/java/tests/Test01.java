da896886148f197ecff987ff66af6adf26da107c
ROMA2 <romasecure84@yandex.ru>
2024-04-18 23:04:04 +0400

    Before test ve after test classlari dersi elave edildi

--- src/test/java/tests/Test01.java
+++ src/test/java/tests/Test01.java
@@ -1,4 +1,31 @@
 package tests;
 
+import org.junit.*;
+
 public class Test01 {
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("Before test class");
+    }
+    @Before
+    public void beforeMyTest(){
+        System.out.println("Before Test");
+    }
+
+    @Test
+    public void myTest1(){
+        System.out.println("test");
+    }
+    @Test
+    public void myTest2(){
+        System.out.println("test 2");
+    }
+    @After
+    public void afterMyTest(){
+        System.out.println("After Test");
+    }
+    @AfterClass
+    public static void afterClass(){
+        System.out.println("After test class");
+    }
 }
